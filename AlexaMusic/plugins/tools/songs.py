3c1b532ee342737623e91ddc8c64a75cc84789c3
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-08-12 08:53:02 +0300

    Update songs.py

--- AlexaMusic/plugins/tools/songs.py
+++ AlexaMusic/plugins/tools/songs.py
@@ -50,7 +50,7 @@ async def song_commad_group(client, message: Message, _):
 # Song Module
 
 
-@app.on_message(filters.command(SONG_COMMAND) & filters.private & ~BANNED_USERS)
+@app.on_message(command(["song","/song", "بحث"]) & filters.private & ~BANNED_USERS)
 @language
 async def song_commad_private(client, message: Message, _):
     await message.delete()
